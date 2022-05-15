8651f81e2f58c94c218741cabe7e7b3a1fbd194f
vicogarcia <garcia.victoria88@gmail.com>
2022-05-14 21:44:17 -0300

    agregando mas funcionalidad a la lista de tareas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  showAddTask:boolean= false;
+  subscription?: Subscription;
+  constructor(private uiService: UiService , private router: Router) {
+    this.subscription= this.uiService.onToggle().subscribe((value) => {this.showAddTask = value})
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-    console.log("toggleAddTask!");
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string){
+    return this.router.url === route
   }
 }
---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/task';
+import { TASKS } from 'src/app/mock-task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+@Input() task: Task = TASKS[0];
+@Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+@Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
+}
